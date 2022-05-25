6a65c1284b1af11ee2a7aadcc080cdd441d9b949
Dukklyon <viaxlux@gmail.com>
2022-05-25 09:46:13 -0300

    Proyecto Final Base de Datos

---
+++ bdepe.sql
@@ -0,0 +1,3 @@
+use control_empleados;
+insert into empleados(apellido, email,nombre) values("Aranguren","vvv","Vito");
+insert into empleados(apellido, email,nombre) values("Aranguren","emma","Emma");
